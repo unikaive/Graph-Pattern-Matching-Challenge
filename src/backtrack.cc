2e540b18c32f82d5f7f3a6d5284272560d3a526f
unikaive <84003457+unikaive@users.noreply.github.com>
2021-06-08 22:36:18 +0900

    Rename Fuc

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -15,13 +15,13 @@ vector<int> matching_order;
 int sum = 0 ;
 int NumOfQueryVertics = 0;
 
-//For query ordering
+//For matching_order
 bool compare(const pair<int,int>& a, const pair<int,int>& b) {
 	if (a.second == b.second) return a.first < b.first;
 	return a.second < b.second;
 }
 
-vector<Vertex> query_ordering(const Graph &query, const CandidateSet &cs){
+vector<Vertex> MatchingOrder(const Graph &query, const CandidateSet &cs){
   map<int, int> m;
   for (int i=0; i < NumOfQueryVertics; i++){
     m[i]=cs.GetCandidateSize(i);
@@ -30,9 +30,6 @@ vector<Vertex> query_ordering(const Graph &query, const CandidateSet &cs){
 	sort(vec.begin(), vec.end(), compare);
 
   vector<int> result;
-  // for (auto num : vec) {
-	// 	cout << "key: "<< num.first << " | value: " << num.second << "\n";
-	// }
   for (auto num : vec) {
 		result.push_back(num.first);
     std::cout<<num.first<<' ';
@@ -60,8 +57,9 @@ void search(const Graph &data, const Graph &query, const CandidateSet &cs, size_
 
     //inspect next candidate
     
-
-    for (int count = 0 ; count < cs.GetCandidateSize(next_query_id); ++count){
+    size_t candidate_size = cs.GetCandidateSize(next_query_id);
+  
+    for (int count = 0 ; count < candidate_size; ++count){
       size_t candidate = cs.GetCandidate(next_query_id,count);
       bool vaild = true ;
       //check candidate is used.
@@ -111,15 +109,10 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
                                 const CandidateSet &cs) {
   ios::sync_with_stdio(false);
 
-  // for (int i = 0; i < query.GetNumVertices(); i++)
-  // {
-  //   matching_order.push_back(i);
-  // }
-  
   map<int, int> result_map; //list for printout
   int now_index = 0 ; //set start
   NumOfQueryVertics = query.GetNumVertices();
-  matching_order = query_ordering(query, cs);
+  matching_order = MatchingOrder(query, cs);
 
   for (int count = 0 ; count < cs.GetCandidateSize(now_index); ++count){
     result_map.clear();
@@ -127,143 +120,7 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
     result_map[now_index]=candidate;
     search(data, query, cs, now_index, result_map);
   }
+
   std::cout<< sum << " ";
 
-
-  // int NumOfQueryVertics = query.GetNumVertices();
-
-  // std::cout << "t " << NumOfQueryVertics << "\n";
-
-  // visited.assign(NumOfQueryVertics, false); // if visited = True,
-  // int start = 0;
-  // index = start;
-  // s.push(index);
-  // cout<< index << "";
-
-  // query.GetNeighbor(index);
-
-  // implement your code here.
-  // for (int count = 0 ; count < NumOfQueryVertics; ++count)
-  //   {
-  //     int temp = cs.GetCandidateSize(count);
-  //     std::cout << temp << "\n";
-  //     for (int count = 0 ; count < temp; ++count)
-  //     {
-  //     int temp2 = cs.GetCandidate(count,count);
-  //     std::cout << temp2 << " ";
-  //     }
-  //   }
-
-
-  //for test
-  // size_t test_vertex = 10 ;
-  // size_t test_label = 78;
-  // std::cout << "GetGraphID : " << query.GetGraphID() << "\n";
-  // std::cout << "GetNumVertices() :" << query.GetNumVertices() << "\n";
-  // std::cout << "GetNumEdges() : " << query.GetNumEdges()<< "\n";
-  // std::cout << "GetNumLabels() : " << query.GetNumLabels() << "\n";
-  // std::cout << "GetLabelFrequency : " << query.GetLabelFrequency(test_label)<< "\n";
-  // std::cout << "GetDegree : " << query.GetDegree(test_vertex)<< "\n";
-  // std::cout << "GetNeighborStartOffset(Vertex v) : " << query.GetNeighborStartOffset(test_vertex) << "\n";
-  // std::cout << "GetNeighborEndOffset(Vertex v) : " << query.GetNeighborEndOffset(test_vertex) << "\n";
-  // std::cout << "GetNeighborStartOffset(Vertex v, Label l)" << query.GetNeighborStartOffset(test_vertex, test_label) << "\n";
-  // std::cout << "GetNeighborEndOffset(Vertex v, Label l)" << query.GetNeighborEndOffset(test_vertex, test_label) << "\n";
-  // std::cout << "GetLabel(Vertex v) : " << query.GetLabel(test_vertex) << "\n";
-  // std::cout << "GetNeighbor(size_t offset) : " << query.GetNeighbor(50) << "\n";
-  // std::cout << "IsNeighbor(Vertex u, Vertex v) :" << query.IsNeighbor(10, 11) << "\n";
-  // for (int i=query.GetNeighborStartOffset(test_vertex); i <query.GetNeighborEndOffset(test_vertex) ; i++){
-  //   std::cout << query.GetNeighbor(i)<< "\n";
-  // }
 }
-
-// void dfs(int start, std::vector<int> graph[], bool check[]){
-// 	check[start]= true;
-// 	printf("%d ", start);
-
-// 	for(int i=0; i < graph[start].size(); i++){
-// 		int next = graph[start][i];
-// 		// 방문하지 않았다면
-// 		if(check[next]==false){
-// 			// 재귀함수를 호출한다.
-// 			dfs(next, graph, check);
-// 		}
-// 	}
-// }
-// void DFS(int i)
-// 	{
-// 		vector<int>::const_iterator next;
-
-// 		visited[i] = true;
-// 		printf("%d\n",i);
-
-// 		for(next = adj[i].begin() ; next != adj[i].end() ; next++) 
-// 		{
-// 			if(visited[*next] != true){				
-// 				DFS(*next);
-// 			}
-// 		}
-
-// 	}
-
- 
-//     }
-//     //check vailidate of candidates
-//     for check next Candidate
-//     if vaildate{
-//       add to result_list
-//       do recursive
-//     }else{
-//       //do nothing, end.
-//     }
-//   }
-//   ---
-  
-
-  
-
-//   //Check Neighbor Vertices of current vertex
-//   for (int i=query.GetNeighborStartOffset(now_vertex); i <query.GetNeighborEndOffset(now_vertex) ; i++){
-//     if (visited[query.GetNeighbor(i)]){
-//       if (i==query.GetNeighborEndOffset(now_vertex)-1){
-
-//       }else{
-//         //Do nothing
-//       }
-//     }else{
-//       search(data, query, cs, now_vertex, visited, result_list);
-//     }
-    
-//   }
-
-//   if (all naibor viertixes is visited){
-//     if (check visted falsed){
-//       search that id
-//     }
-//     else{ //if all visted is true, print result
-//       for (int count = 0; count < query.GetNumVertices();++count){
-//         std::cout << result_list[count] << " ";
-//         }
-//       std::cout <<  "\n";
-//     }
-//   }else{
-//     search naibor vertices
-    
-//   }
-//   //if done, print result_list
-//   if (now_vertex == query.GetNumVertices()){
-//     for (int count = 0; count < query.GetNumVertices();++count){
-//       std::cout << result_list[count] << " ";
-//       }
-//     std::cout <<  "\n";
-//     sum = sum+1;
-
-//   }else{
-//   // else put element to result_list, and do recursive.
-//     for (int count = 0 ; count < cs.GetCandidateSize(now_vertex); ++count)
-//     {
-//     int temp2 = cs.GetCandidate(now_vertex,count);
-//     result_list[now_vertex] = temp2;
-//     search(data, query, cs, now_vertex+1, visited, result_list);
-//     }
-//   }
-// }
